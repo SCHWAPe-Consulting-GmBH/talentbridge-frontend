acb29c3aeecdaa20ce503d0d1ba6b12c7cfe1128
Katrin <katrinmatyash01@gmail.com>
2024-10-31 18:14:15 +0200

    change project context

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './styles/index.css';
-import { App }from './App';
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
