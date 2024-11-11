630519db4921b1a85f3146407043d39061d15d5e
Katrin <katrinmatyash01@gmail.com>
2024-11-11 11:18:57 +0200

    change Readme

--- src/app/about-courses/registration/page.tsx
+++ src/app/about-courses/registration/page.tsx
@@ -2,7 +2,9 @@
 
 import { useState } from 'react';
 import cn from 'classnames';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import { Select } from 'antd';
+import { ReactComponent as ArrowIcon } from '@/assets/icons/arrow_select.svg';
 import { RegistrationNotification } from '@/components/registrationNotification';
 
 const months = [
@@ -22,10 +24,19 @@ const months = [
 
 const Registration = () => {
   const [isModalShown, setIsModalShown] = useState(false);
+  const [scheduledDate, setSchedulateDate] = useState({
+    month: '',
+    day: 0,
+    hours: ''
+  })
 
   const handleOpenModal = () => {
     setIsModalShown(true);
   };
+
+  const handleChangeDate = (value: string | number, field: string) => {
+    setSchedulateDate(prevDate => ({...prevDate, [field]: value}));
+  }
 
   return (
     <div className="pt-[100px] px-[100px] max-w-[1440px] mx-auto relative">
@@ -73,9 +84,23 @@ const Registration = () => {
             <h2 className="text-[36px] font-semibold text-themetext mb-8">
               Choose a date
             </h2>
-            
+
             <div className="flex ">
-              {/* тут має бути select */}
+              <ArrowIcon height={25} width={25} fill="pink" stroke="#0066ff"/>
+              {/* <Select
+                defaultValue="lucy"
+                style={{ width: 120 }}
+                placeholder='month'
+                suffixIcon={<ArrowIcon height={25} width={25} fill="pink" stroke="#0066ff"/>}
+                onChange={e => handleChangeDate(e, 'month')}
+                options={[
+                  { value: 'jack', label: 'Jack' },
+                  { value: 'lucy', label: 'Lucy' },
+                  { value: 'Yiminghe', label: 'yiminghe' },
+                  { value: 'disabled', label: 'Disabled', disabled: true },
+                ]}
+              /> */}
+              {/* <p>тут має бути select </p> */}
             </div>
           </div>
         </div>
