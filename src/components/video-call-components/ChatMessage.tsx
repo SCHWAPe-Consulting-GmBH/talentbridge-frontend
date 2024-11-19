aeb3671fd6a273eb64a8714c455787460c6ef9ce
ruslanhal <r.halushka.work@gmail.com>
2024-11-19 17:35:41 +0200

    added real time chat with firestore

---
+++ src/components/video-call-components/ChatMessage.tsx
@@ -0,0 +1,28 @@
+import { auth } from '@/firebase/config';
+
+const ChatMessage = (props) => {
+  const { text, uid, photoURL } = props.message;
+  const isSent = uid === auth.currentUser.uid;
+  const containerClass = isSent
+    ? 'flex-row-reverse bg-green-500'
+    : 'bg-gray-500';
+  const imgClass = isSent ? 'ml-2' : 'mr-2';
+  const textAlignClass = isSent ? 'text-right' : 'text-left';
+
+  return (
+    <div
+      className={`flex items-center ${containerClass} p-2 rounded-lg my-1 mx-3 max-w-xs md:max-w-md lg:max-w-lg ${isSent ? 'ml-auto' : 'mr-auto'}`}
+    >
+      <img
+        src={
+          photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'
+        }
+        alt="User Avatar"
+        className={`rounded-full w-10 h-10 object-cover ${imgClass}`}
+      />
+      <p className={`flex-1 text-sm text-white ${textAlignClass}`}>{text}</p>
+    </div>
+  );
+};
+
+export default ChatMessage;
